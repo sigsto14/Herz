3ace6c631928e4093253c8d9a98293ac358bee1a
sigsto14 <Sigridstorck@yahoo.se>
2016-03-10 13:00:26 +0100

    Index
    Lagt till innehåll i index (jag råkade ta bort det innan I'm sorry)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,24 @@
 @extends('template')
-
 @section('container')
 @section('footer')
 
    
         <title>Herz</title>
 
-          
+            </div>
+        </div>
+
+        <div class="container">
+          <div class="podcast-box">
+          <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#">Populärt just nu</a></li>
+            <li role="presentation"><a href="#">Veckans pod</a></li>
+            <li role="presentation"><a href="#">Senast uppladat</a></li>
+          </ul> 
+        </div>
+    </div> 
+
+
+    </body>
+
 @stop
