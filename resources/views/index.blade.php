595c8539ad29cb8c49eae094d7f4d0cbf8b28524
alebal14 <alexandra.b.balogh@gmail.com>
2016-03-13 17:04:11 +0100

    update -css
    innehåll box

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,31 @@
         </div>
 
         <div class="container">
-          <div class="podcast-box" id="container">
+          <div class="podcast-box">
           <ul class="nav nav-tabs" role="tablist">
             <li role="presentation" class="active"><a href="#">Populärt just nu</a></li>
             <li role="presentation"><a href="#">Veckans pod</a></li>
             <li role="presentation"><a href="#">Senast uppladat</a></li>
-          </ul> 
+          </ul>
+          <div class="col-md-12" id="container2">
+          <div class="row">
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+          </div>
+          <br>    
+          <div class="row">
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>    
+          </div>
+          <br>    
+          <div class="row">
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>
+              <div class="col-md-4"><img src="images/podcast_av/pod.png"></div>    
+          </div> 
+         </div>  
         </div>
     </div> 
 
