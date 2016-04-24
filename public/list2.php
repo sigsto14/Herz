6496911e23446c7203c8cc4c8db8685007905c7c
sigsto14 <Sigridstorck@yahoo.se>
2016-04-24 22:44:45 +0200

    finally
    fix på spellistor på user show

---
+++ public/list2.php
@@ -0,0 +1,58 @@
+<?php
+
+
+$mysqli = new mysqli("localhost","root","","herz");
+
+$ADD = '';
+
+$str ='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<music>';
+
+$listID = $_GET['listID2'];
+$userID = $_GET['userID2'];
+
+$query = <<<END
+SELECT * FROM playlists
+WHERE listID = '{$listID}'
+
+
+END;
+$res = $mysqli->query($query);
+if($res->num_rows > 0){
+  $playlist = $res->fetch_object();
+
+  $listItems = array_values(explode(',',$playlist->soundIDs,13));
+  foreach($listItems as $listItem){
+
+	$query2 = <<<END
+	SELECT * FROM sounds
+	WHERE soundID = '{$listItem}'
+END;
+$res2 = $mysqli->query($query2);
+if($res2->num_rows > 0){
+	
+	$URLS = $res2->fetch_object(); 
+}
+
+	$ADD .= '<song url="' . $URLS->URL .'"/>
+	';
+
+}
+}
+
+
+$str .= $ADD;
+$str .= '</music>';
+
+
+
+echo $str;
+
+$file_name="list.xml"; // file name
+$fp = fopen ($file_name, "w"); 
+
+fwrite ($fp,$str); 
+fclose ($fp); 
+chmod($file_name,0777); 
+
+?>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -138,68 +138,328 @@ Your browser does not support the audio element.
 
   <!-- lite kod för att hämta ut användarens spellistor -->
   <h1>Spellistor</h1>
-  <?php
+ <?php
+/* behöver fylla en array senare */
+  $array = '';
+  /* hämtar playlists*/
 $playlists = DB::table('playlists')->where('userID', '=', $user->userID)->orderBy('created_at', 'ASC')->take(5)->get();
+/*kollar hur många resultat som går att ta ut så att vi kan använda detta i if-satser senare */
+$count = DB::table('playlists')->where('userID', '=', $user->userID)->orderBy('created_at', 'ASC')->take(5)->count();
+/* en variabel för o kolla om det är tomt */
 $playlistsCheck = DB::table('playlists')->where('userID', '=', $user->userID)->first();
+/* en foreach loop för att sätta in värden i vår array */
+foreach($playlists as $playlist){
+if($playlist->listID == $playlistsCheck->listID){
+  /* det första värdet ska ej ha , */
+$array .= $playlist->listID;
 
+}
+else{
+/* de andra värdena ska skiljas med , */
+$array .= ',' . $playlist->listID;
 
-  ?>
+}
+}
+/* gör en array av värdena vi satt in i variabeln för arrayen */
+$lists = array_values(explode(',',$array,10));
+
+/* med hjälp av att kolla hur många resultat vi fick gör vi variabler av värdena i arrayen */
+/* variabler för var resultat (max 5) */
+/* detta för att javascript och ny xml ej fungerar med foreach loop och id'n */
+if($count > 0){
+$listID1 = $lists[0]; 
+$list1 = DB::table('playlists')->where('listID', '=', $listID1)->first();
+}
+if($count > 1){
+$listID2 = $lists[1];
+$list2 = DB::table('playlists')->where('listID', '=', $listID2)->first();
+}
+if($count > 2){
+$listID3 = $lists[2];
+$list3 = DB::table('playlists')->where('listID', '=', $listID3)->first();
+}
+if($count > 3){
+$listID4 = $lists[3];
+$list4 = DB::table('playlists')->where('listID', '=', $listID4)->first();
+}
+if($count > 4){
+$listID5 = $lists[4];
+$list5 = DB::table('playlists')->where('listID', '=', $listID5)->first();
+}
+ ?>
+ <!-- om det är tomt ska det stå -->
   @if(is_null($playlistsCheck))
   <p>Oj oj, här var det tomt! {{ $user->username }} har inga spellistor än</p>
   @else
-<ul class="nav nav-tabs" role="tablist" >
-            <li role="presentation" class="active"><a href="#senaste" role="tab" data-toggle="tab">Senaste</a></li>
- 
-  @foreach($playlists as $playlist)
-     <li class="nav nav-tabs" role="tablist"><a href="#{{ $playlist->listID }}" role="tab" data-toggle="tab">{{ $playlist->listTitle }}</a></li>
-        
 
+<!-- annars kör samma if-satser som när variablerna gjordes och skriva ut varje värde för sig -->
 
-
-  @endforeach
-</ul>
-      @foreach($playlists as $playlist)
-
-  <div class="tab-content">
-<div role="tabpanel" class="tab-pane" id="{{ $playlist->listID }}">
-         
-  <h2><a href="http://localhost/Herz/public/playlist/{{ $playlist->listID }}">{{ $playlist->listTitle }}</a></h2>
+    @if($count > 0)
+<!-- första resultatet -->
+<div class="row" id="list1">
+<!-- lite länkar o lista -->
+  <h2><a href="http://localhost/Herz/public/playlist/{{ $list1->listID }}">{{ $list1->listTitle }}</a></h2>
    <br>
-     <h4>Beskrivning</h4><br><p>{{ $playlist->listDescription }}</p>
-
+     <h4>Beskrivning</h4><br><p>{{ $list1->listDescription }}</p>
+<!-- en box som vi ska ladda in värden i senare -->
    <div id="box1">
 
-    <form action="" method="put" name="play" id="play">
-<input type="hidden" name="listID" value="{{ $playlist->listID }}" id="listID">
+    <form action="" method="put" name="play1" id="play">
+<input type="hidden" name="listID" value="{{ $list1->listID }}" id="listID">
     <button type="submit" class="btn btn-default btn-lg" id="play">
+  <span class="glyphicon glyphicon-expand" aria-hidden="true"></span>
+</button>
+ </form>  </div>
+<!-- om man äger spellistan kan man radera den -->
+@if(Auth::check())
+@if(Auth::user()->userID == $user->userID )
+{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $listID1))) !!}
+{!! csrf_field() !!}
+{!! Form::submit('X', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Säker på att du vill ta bort spellistan?");' )) !!}
+{!! Form::close() !!}
+@endif
+@endif
+<!-- ett script som laddar in spelare i box1 och skickar värden med ajax till en php-funktion som gör xml-fil så att värdena i spelaren blir rätt -->
+<script>
+
+$('#play').submit(function(e){
+e.preventDefault();
+$("#box1").load( "http://localhost/Herz/public/player.html" );
+var listID = $('#listID').val();
+   var listID = $.trim(listID);
+     var userID = $('#userID').val();
+   var userID = $.trim(userID);
+ 
+$.ajax({
+
+       url: 'http://localhost/Herz/public/list.php',
+       data: { listID: listID, userID: userID},
+       dataType: 'json',
+       success: function(data){
+            
+       }
+    });
+ 
+
+});
+</script>
+</div>
+ 
+@endif
+  <!-- repeat -->
+ @if($count > 1)
+
+<div class="row" id="list2">
+  <h2><a href="http://localhost/Herz/public/playlist/{{ $list2->listID }}">{{ $list2->listTitle }}</a></h2>
+   <br>
+     <h4>Beskrivning</h4><br><p>{{ $list2->listDescription }}</p>
+
+   <div id="box2">
+
+    <form action="" method="put" name="play2" id="play2">
+<input type="hidden" name="listID2" value="{{ $list2->listID }}" id="listID2">
+    <button type="submit" class="btn btn-default btn-lg" id="play2">
   <span class="glyphicon glyphicon-expand" aria-hidden="true"></span>
 </button>
  </form>  </div>
 
 @if(Auth::check())
 @if(Auth::user()->userID == $user->userID )
-{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $playlist->listID))) !!}
+{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $listID2))) !!}
 {!! csrf_field() !!}
 {!! Form::submit('X', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Säker på att du vill ta bort spellistan?");' )) !!}
 {!! Form::close() !!}
 @endif
 @endif
+<script>
 
-  </div>
-  </div>
-
+$('#play2').submit(function(e){
+e.preventDefault();
+$("#box2").load( "http://localhost/Herz/public/player.html" );
+var listID2 = $('#listID2').val();
+   var listID2 = $.trim(listID2);
+     var userID2 = $('#userID2').val();
+   var userID2 = $.trim(userID2);
  
-   @endforeach
-   
-  
+$.ajax({
+
+       url: 'http://localhost/Herz/public/list2.php',
+       data: { listID2: listID2, userID2: userID2},
+       dataType: 'json',
+       success: function(data){
+          
+       }
+    });
+ 
+
+});
+</script>
+</div>
+ 
+@endif
+
+
+<!-- repeat -->
+@if($count > 2)
+
+<div class="row" id="list3">
+  <h2><a href="http://localhost/Herz/public/playlist/{{ $list3->listID }}">{{ $list3->listTitle }}</a></h2>
+   <br>
+     <h4>Beskrivning</h4><br><p>{{ $list2->listDescription }}</p>
+
+   <div id="box3">
+
+    <form action="" method="put" name="play3" id="play3">
+<input type="hidden" name="listID3" value="{{ $list3->listID }}" id="listID3">
+    <button type="submit" class="btn btn-default btn-lg" id="play3">
+  <span class="glyphicon glyphicon-expand" aria-hidden="true"></span>
+</button>
+ </form>  </div>
+
+@if(Auth::check())
+@if(Auth::user()->userID == $user->userID )
+{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $listID3))) !!}
+{!! csrf_field() !!}
+{!! Form::submit('X', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Säker på att du vill ta bort spellistan?");' )) !!}
+{!! Form::close() !!}
+@endif
+@endif
+<script>
+
+$('#play3').submit(function(e){
+e.preventDefault();
+$("#box3").load( "http://localhost/Herz/public/player.html" );
+var listID3 = $('#listID3').val();
+   var listID3 = $.trim(listID3);
+     var userID3 = $('#userID3').val();
+   var userID3 = $.trim(userID3);
+ 
+$.ajax({
+
+       url: 'http://localhost/Herz/public/list3.php',
+       data: { listID3: listID3, userID3: userID3},
+       dataType: 'json',
+       success: function(data){
+            
+       }
+    });
+ 
+
+});
+</script>
+</div>
+ 
+@endif
+<!-- repeat -->
+@if($count > 3)
+
+<div class="row" id="list4">
+  <h2><a href="http://localhost/Herz/public/playlist/{{ $list4->listID }}">{{ $list4->listTitle }}</a></h2>
+   <br>
+     <h4>Beskrivning</h4><br><p>{{ $list4->listDescription }}</p>
+
+   <div id="box4">
+
+    <form action="" method="put" name="play4" id="play4">
+<input type="hidden" name="listID4" value="{{ $list4->listID }}" id="listID4">
+    <button type="submit" class="btn btn-default btn-lg" id="play4">
+  <span class="glyphicon glyphicon-expand" aria-hidden="true"></span>
+</button>
+ </form>  </div>
+
+@if(Auth::check())
+@if(Auth::user()->userID == $user->userID )
+{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $listID4))) !!}
+{!! csrf_field() !!}
+{!! Form::submit('X', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Säker på att du vill ta bort spellistan?");' )) !!}
+{!! Form::close() !!}
+@endif
+@endif
+<script>
+
+$('#play4').submit(function(e){
+e.preventDefault();
+$("#box4").load( "http://localhost/Herz/public/player.html" );
+var listID4 = $('#listID4').val();
+   var listID4 = $.trim(listID4);
+     var userID4 = $('#userID4').val();
+   var userID4 = $.trim(userID4);
+ 
+$.ajax({
+
+       url: 'http://localhost/Herz/public/list4.php',
+       data: { listID4: listID4, userID4: userID4},
+       dataType: 'json',
+       success: function(data){
+            
+       }
+    });
+ 
+
+});
+</script>
+</div>
+ 
+@endif
+<!-- repeat -->
+@if($count > 4)
+
+<div class="row" id="list5">
+  <h2><a href="http://localhost/Herz/public/playlist/{{ $list5->listID }}">{{ $list5->listTitle }}</a></h2>
+   <br>
+     <h4>Beskrivning</h4><br><p>{{ $list5->listDescription }}</p>
+
+   <div id="box5">
+
+    <form action="" method="put" name="play5" id="play5">
+<input type="hidden" name="listID5" value="{{ $list5->listID }}" id="listID5">
+    <button type="submit" class="btn btn-default btn-lg" id="play5">
+  <span class="glyphicon glyphicon-expand" aria-hidden="true"></span>
+</button>
+ </form>  </div>
+
+@if(Auth::check())
+@if(Auth::user()->userID == $user->userID )
+{!!   Form::open(array('method' => 'DELETE', 'route' => array('playlist.destroy', $listID5))) !!}
+{!! csrf_field() !!}
+{!! Form::submit('X', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Säker på att du vill ta bort spellistan?");' )) !!}
+{!! Form::close() !!}
+@endif
+@endif
+<script>
+
+$('#play5').submit(function(e){
+e.preventDefault();
+$("#box5").load( "http://localhost/Herz/public/player.html" );
+var listID5 = $('#listID5').val();
+   var listID5 = $.trim(listID5);
+     var userID5 = $('#userID5').val();
+   var userID5 = $.trim(userID5);
+ 
+$.ajax({
+
+       url: 'http://localhost/Herz/public/list5.php',
+       data: { listID5: listID5, userID5: userID5},
+       dataType: 'json',
+       success: function(data){
+            
+       }
+    });
+ 
+
+});
+</script>
+</div>
+ 
+@endif
 
   @endif
-  <!-- Innehåll här (List) -->
+
   </div>
 </div>
 
 </div>
-<!-- slut på rekommendationer -->
+
    
        
         </div>
