b1dca7b60bd59172a2080243b250796634386a65
Louise <louka93@gmail.com>
2016-04-26 12:15:49 +0200

    Vad är Herz mindre red

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,14 +2,15 @@
 @extends('template')
 @section('container')
 @section('footer')
+
 <!DOCTYPE HTML>
-    <body>
-    	@yield('content')
-    	<!-- Kanal innehåll --> 
+
+<body>
+	@yield('content')
     <div class="container">
     <div class="col-md-12" id="container">
         <h1>Vad är Herz</h1>
         <p>Herz är en plattform för dig med intresse för podcasts. På sidan kan du ladda upp eget material eller lyssna på andras.</p>
     </div>
     </div>
-    </body>
+</body>
