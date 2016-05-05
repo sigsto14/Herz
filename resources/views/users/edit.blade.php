a081cd349c01df381f27a3c85d3984b5fd2003fa
alebal14 <alexandra.b.balogh@gmail.com>
2016-05-05 11:19:58 +0200

    user edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -7,70 +7,77 @@
 <title>Edit Profile</title>
 </head>
 <div class="container">
-<div class="col-md-12"id="container">
-<br><br><br><br><br><br><br><br><br><br><br>
-<table class="table">
+<div class="col-md-3"></div>
+<div class="col-md-6" id="mini-container">
+
+
+<h1>Redigera Profil</h1>
+
 @if(Auth::check())
+<div class="section">Redigera Info</div>
+    <div class="inner-wrap">
+
+
 @if(Auth::user()->userID == $user->userID)
 {{-- formulär som även visar den data som är i databasen, för ändringar i users och channels tabeller--}}
 {!!     Form::model($user, array('route' => array('user.update', $user->userID), 'files' => 'true', 'method' => 'PUT')) !!}
 {!! csrf_field() !!}
-{!!     Form::label('username', 'Username:') !!}
-{!!     Form::text('username') !!}<br>
+{!!     Form::label('username', 'Användarnamn:') !!}
+{!!     Form::text('username') !!}
 
 
 
 
 {!!     Form::label('email', 'Email:') !!}
-{!!     Form::text('email') !!}<br>
+{!!     Form::text('email') !!}
+
+{!! Form::label('Profilbild') !!}<br>
+<img src="{{ $user->profilePicture }}" style="width:145px;height:159px;"/>
+
+{!! Form::file('image', null) !!}
 
 
-<img src="{{ $user->profilePicture }}" style="width:145px;height:159px;"/><br>
-{!! Form::label('Profilbild') !!}
-{!! Form::file('image', null) !!}<br>
 
-
-</table>
 
 <input type="submit" class="btn" value="Uppdatera">
 
-<br>
-{!!     Form::close() !!}<br><br>
-<br>
-<br>
+
+{!!     Form::close() !!}
+
+
+</div>
 
 @if(Session::has('message'))
 <div class="alert alert-success">
 	{{ Session::get('message') }}
 </div>
+
 @endif
 @if(Session::has('message1'))
 <div class="alert alert-danger">
 	{{ Session::get('message1') }}
 </div>
+
+
 @endif
-<button type="button" id="changePass" class="btn btn-success">Byt Lösenord</button>
+<div class="section">Byt Lösenord</div>
+    <div class="inner-wrap">
+
 <form action="http://localhost/Herz/public/user/resetPass" method="POST">
 {!! csrf_field() !!}
-<div id="newPass" class="hidden">
-<label>Nytt lösenord</label><input type="password" id="newPass" name="newPass"><br>
+<div id="newPass">
+<label>Nytt lösenord:</label><input type="password" id="newPass" name="newPass">
 
 {!!     Form::label('newPassConfirm', 'Upprepa Nytt Lösenord:') !!}
-{!!     Form::password('newPassConfirm') !!}<br>
+{!!     Form::password('newPassConfirm') !!}
 
 {!!     Form::label('activeConfirm', 'Nuvarande Lösenord:') !!}
-{!!     Form::password('activeConfirm') !!}<br>
+{!!     Form::password('activeConfirm') !!}
 <input type="submit" class="btn" value="Byt lösenord">
 </div>
 
 {!!     Form::close() !!}
- <script>
-$('#changePass').click(function(){
-  $("#newPass").toggleClass("hidden");
-  
-
-});
-</script>
+</div>
 @if(Session::has('message3'))
 <div class="alert alert-success">
 	{{ Session::get('message3') }}
@@ -82,11 +89,15 @@ $('#changePass').click(function(){
 </div>
 @endif
 
+<div class="section">Radera konton</div>
+    <div class="inner-wrap">
+
 {!! Form::open(array('method' => 'DELETE', 'route' => array('user.destroy', $user->userID))) !!}
 
 	{!! Form::submit('Ta bort konto', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Är du säker på att du vill ta bort ditt konto?");')) !!}
 {!!     Form::close() !!}
 
+</div>
 
 
 @else
