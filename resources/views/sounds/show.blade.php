6f70e74343d32167a6d0ee0ee63c90d5cafa7652
zeiex <tobias.isacson@live.se>
2016-05-05 20:27:58 +0200

    Dela på Facebook knapp

--- resources/views/sounds/show.blade.php
+++ resources/views/sounds/show.blade.php
@@ -51,6 +51,16 @@ chmod($file_name2,0777);
 
 <title>{{ $sound->title }}</title>
 <body>
+<!-- Facebook JS -->  
+  <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
 @yield('content')
 <!-- Sound show innehåll --> 
   <div class="container">
@@ -193,6 +203,8 @@ END;
               </li>
               <!--  Favorit mätare slut -->            
             </ul>
+            <!-- Knapp för att dela på Facebook -->
+            <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-mobile-iframe="true"></div>
           </div>
            <!--  Podmenubar slut --> 
             <!--  Podinfo box --> 
