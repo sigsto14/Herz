fdcca73717cb21c13c5aa83ddbc1c63f45df6cfb
alebal14 <alexandra.b.balogh@gmail.com>
2016-05-06 21:57:19 +0200

    search

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -145,38 +145,39 @@ $titleNr = '';
               </ul>
             </li>
             <!-- Sökfunktion -->
-
-
-            <div id="search" >  
-              <form class="form-wrapper cf" action="http://localhost/Herz/public/search/index">
-                <div class="input-group">
-                  <!-- Sökfältet -->
-                  <div class="left-inner-addon ">
-    <i class="icon-search"></i>
-                  <input type="search" class="form-control" placeholder="Sök" name="search" value="Sök" onfocus="if(this.value  == 'Sök') { this.value = ''; } " onblur="if(this.value == '') { this.value = 'Sök'; } "> </div>
-      
-                  <button type="submit" value="Sök">Sök</button>
-                  <div class="input-group-btn">
-                                  <!-- Kategorier i sökfältet -->
+<div id="search" >  
+              <form action="http://localhost/Herz/public/search/index">
+              <div class="input-group">
+            <div class="input-group-btn">
+              <button tabindex="-1" data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button">
+              <span class="caret"></span>
+              </button>
+              <ul role="input" class="dropdown-menu">
+              <script type="text/javascript">$('.dropdown-menu').click(function(e) {
+    e.stopPropagation();
+});</script>
+                                   <!-- Kategorier i sökfältet -->
                   <!-- gör php för att hämta ut kategorierna-->
                   <?PHP
                   $categories = DB::table('category')->orderBy('categoryname', 'asc')->get();
                   ?>
-                  <div class="blabla">
-                  <div class="test">
-                  <select name="categoryID" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="kat">
-                  <option selected="selected" disabled="disabled">Kategorier</option>                     @foreach($categories as $category)
-                     
-                  <option value="{{ $category->categoryID }}">{{$category->categoryname}}</option>
+                    
+                    @foreach($categories as $category)
+                     <li><a>
+                      <input type="checkbox" value="{{ $category->categoryID }}"><span class="lbl">{{$category->categoryname}}</span></a>
+                      </li>
                  @endforeach
               
-                </select></div>      
-                </div><!-- /btn-group -->
-
-              </div><!-- /input-group -->
-
-            </form>
-            </div>
+        
+              </ul>
+             </div>
+            <input type="search" class="form-control" name="search"/>
+            <div class="input-group-btn">
+            <button tabindex="-1" class="btn btn-default" type="submit">Sök</button></div>
+          </div>
+</form>
+          </div>
+            
               <!-- Sökfältet slut -->
           </ul><!-- Vänster delen av naven slut -->
 
