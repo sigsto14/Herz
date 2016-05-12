9a12b3d655de287ba152e73ab19978426d1aa4b0
alebal14 <alexandra.b.balogh@gmail.com>
2016-05-12 21:09:47 +0200

    prenumerationer

--- resources/views/subscribe/index.blade.php
+++ resources/views/subscribe/index.blade.php
@@ -18,14 +18,14 @@ $subscribes = DB::table('subscribe')->where('subscribe.userID', '=', $userID)->j
 
 <body>
 @yield('content')
-
-   <h2>{{ Auth::user()->username }}'s prenumerationer</h2>
+<br>
+   <h1 id="uc-title">{{ Auth::user()->username }}'s prenumerationer</h1>
    <!-- lägger ut resultaten en och en -->
-
+   <div class="row" id="scroll">
     @foreach($subscribes as $subscribe)
     <!-- gör variabel inuti foreachen för att kunna hämta ut ur sounds-tabellen som stämmer överens, kollar om kanal har sound annars text att den saknar -->
 <?php
-$uploads = DB::table('subscribe')->join('sounds', 'sounds.channelID', '=', 'subscribe.channelID')->where('sounds.channelID','=',$subscribe->channelID)->orderBy('sounds.created_at', 'ASC')->take(1)->get();
+$uploads = DB::table('subscribe')->join('sounds', 'sounds.channelID', '=', 'subscribe.channelID')->join('channels', 'channels.channelID', '=', 'sounds.channelID')->where('sounds.channelID','=',$subscribe->channelID)->orderBy('sounds.created_at', 'ASC')->take(1)->get();
 
 $mysqli = new mysqli("localhost","root","","herz");
 $channelID = $subscribe->channelID;
@@ -44,46 +44,38 @@ else {
 $state = 0;
 }
 ?>
-              <div class="row">
-              <div class="col-md-4">
-                <a href="http://localhost/Herz/public/channel/{{ $subscribe->channelID }}"><h1>{{ $subscribe->channelname }}</h1></a>
-              
-@if($state == 1)
-@foreach($uploads as $upload)
 
-<p>Senaste uppladdning</p><br>
-<p>{{ $upload->created_at }}</p>
-<h3>{{ $upload->title }}</h3>
-              <image src="{{ $upload->podpicture }}" width="100px" height="auto"></image><br>
-              <audio controls>
-              <source src="{{ $upload->URL }}" type="audio/ogg">
-              <source src="{{ $upload->URL }}" type="audio/mpeg">
-              Your browser does not support the audio element.
-              </audio>
-
-
-@endforeach
-@else
+              <div class="col-md-3" id="uc-box">
+              @if($state == 1)
+@foreach($uploads as $upload)          
+               <a href="http://localhost/Herz/public/sound/{{ $upload->podpicture }}"><image src="{{ $upload->podpicture }}" width="150px" height="150" id="pod-mini-img"></a>
+               <div class="podtitle-box">
+               <a href="http://localhost/Herz/public/sound/{{ $upload->soundID }}"><h4>{{ $upload->title }}</h4></a>
+               <div class="podtitledownbox">
+              <div class="podfavicon2">
+                <div class="vertical-line2" style="margin-left:-60%; "></div>
+                 <p style="margin-top: -3%; margin-left:-55%; font-size: 9px;">{{ $upload->created_at }}</p>
+                </div>     
+            <div class="podchanneltitle">              
+              <p style="margin-left: 6%; margin-top: -3%; font-size: 10px;">av <a href="http://localhost/Herz/public/channel/{{ $upload->channelID }}" style="font-size: 10px;">{{ $upload->channelname }}</a></p>
+              </div>
+              </div>
+              </div>
+              </div>
+                       @endforeach
+         @else
 <p>Denna kanal har inga ljudklipp än!</p>
-@endif
-              </div>           
-
-  
-
-         @endforeach
-         
-
-
-    
-        </div>
-        </div>
-</div>
-</div>        
-     
+@endif 
+     @endforeach
+      </div>
+     </div>
+     </div>
    
-		@endif	
 
+    @endif  
 
-
+</div>
+</div>
+</div>
 </body>
 @stop
